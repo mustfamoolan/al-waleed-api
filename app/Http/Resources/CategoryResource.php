0cdc1b491eee5aca064d1fa1edf1dc1310789e2b
mustfamoolan <almoq3m@gmail.com>
2026-02-14 04:22:14 +0300

    Fix duplicate migrations causing server crash

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -15,6 +15,8 @@ class CategoryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
+            'name' => $this->name,
             'category_id' => $this->id,
             'category_name' => $this->name,
             'description' => $this->description,
